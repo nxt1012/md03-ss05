112640a84ae5a0a3ed47253fdf8dc7cbd104bc73
Nguyễn Xuân Thành <nxt1012@gmail.com>
2023-09-20 22:19:21 +0700

    update exercises for session 5

--- src/ra/th3/Main.java
+++ src/ra/th3/Main.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         try {
-            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            URL url = new URL("https://tintuc.vn/xa-hoi");
 //            open the stream and put it into BufferedReader
             Scanner sc = new Scanner(new InputStreamReader(url.openStream()));
             sc.useDelimiter("\\Z");
@@ -24,11 +24,17 @@ public class Main {
             content = content.replaceAll("\n+", "");
 //            đổi pattern mới vì pattern cũ đã không còn sử dụng được
 //            regex
-            Pattern p = Pattern.compile(";\\\"(.*)<\\/a>");
-            Matcher m = p.matcher(content);
-            while (m.find()){
-                System.out.println(m.group(1));
+            Pattern p1 = Pattern.compile("<div class=\"w-body list page24h\">(.*?)</div></div></div>");
+            Matcher m1 = p1.matcher(content);
+            while(m1.find()){
+                String filteredContent = m1.group(1);
+                Pattern p2 = Pattern.compile("title=\"(.*?)\">");
+                Matcher m2 = p2.matcher(filteredContent);
+                while (m2.find()){
+                    System.out.println(m2.group(1));
+                }
             }
+
 
         } catch (MalformedURLException e){
             e.printStackTrace();
